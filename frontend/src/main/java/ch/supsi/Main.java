357a3d152a0b16807dee20ec37b29508ecdfde7f
Matteo <matteo.metaldi@student.supsi.ch>
2021-10-18 12:48:06 +0200

    add main application

--- frontend/src/main/java/ch/supsi/Main.java
+++ frontend/src/main/java/ch/supsi/Main.java
@@ -2,6 +2,6 @@ package ch.supsi;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello wolrd!");
+        MainApplication.main(args);
     }
 }
---
+++ frontend/src/main/java/ch/supsi/MainApplication.java
@@ -0,0 +1,23 @@
+package ch.supsi;
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.layout.GridPane;
+import javafx.stage.Stage;
+
+import java.util.Locale;
+
+public class MainApplication extends Application {
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        Locale.setDefault(Locale.ITALIAN);
+
+        stage.setTitle("Scheduler");
+        stage.setScene(new Scene(new GridPane(), 800, 500));
+        stage.show();
+    }
+}
