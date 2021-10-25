91733ca9180dbdbe8cddbb98ebcbcfaaecda3b71
Diego Moranda <diego.moranda@supsi.ch>
2021-10-25 15:08:15 +0200

    ProcessListView refactoring

--- frontend/src/main/java/ch/supsi/ProcessListView.java
+++ frontend/src/main/java/ch/supsi/ProcessListView.java
@@ -15,30 +15,42 @@ import javafx.scene.text.Font;
 
 public class ProcessListView {
     VBox container = new VBox();
+    Button addBtn = new Button("Add new process");
     long id = 0;
 
     public ProcessListView() {
         container.setSpacing(16);
         container.setTranslateX(20);
         container.setTranslateY(20);
+        container.getChildren().add(addBtn);
+        addBtn.setOnMouseClicked(mouseEvent -> add(new Process("Process "+id)));
     }
 
     public void add(Process process){
-        HBox cell = new HBox();
-        Label label = new Label();
-        Button button = new Button();
-        Region space = new Region();
         String hexColor = "#83C1DC";
 
-        space.setPrefWidth(200);
-        space.setPrefHeight(30);
-        HBox.setHgrow(space, Priority.ALWAYS);
+        HBox cell = getCell(hexColor);
+        Label label = getLabel(process.toString());
+        Button button = getButton();
+        Region space = getSpace();
 
         if(isDark(hexColor))
             label.setStyle("-fx-text-fill: #ffffff;");
         else
             label.setStyle("-fx-text-fill: #000000;");
 
+        cell.getChildren().addAll(label,space,button);
+        
+        container.getChildren().add(cell);
+    }
+
+    public void removeCell(String id){
+        Node toRemove =  container.lookup('#'+id);
+        container.getChildren().remove(toRemove);
+    }
+
+    private HBox getCell(String hexColor){
+        HBox cell = new HBox();
         cell.setStyle("-fx-background-radius: 5; -fx-background-color: "+hexColor+";");
         cell.setEffect(new DropShadow(10, Color.BLACK));
         cell.setPrefHeight(32);
@@ -46,28 +58,38 @@ public class ProcessListView {
         cell.setFillHeight(false);
         cell.setId("cell"+id);
         id++;
+        return cell;
+    }
+
+    private Label getLabel(String text){
+        Label label = new Label(text);
+        label.setFont(Font.font("Futura"));
+        label.setPrefWidth(150);
+        label.setAlignment(Pos.CENTER);
+        return label;
+    }
+
+    public Region getSpace(){
+        Region space = new Region();
+        space.setPrefWidth(200);
+        space.setPrefHeight(30);
+        HBox.setHgrow(space, Priority.ALWAYS);
+        return space;
+    }
+
+    private Button getButton(){
+        Button button = new Button();
 
         button.setText("Remove");
         button.setStyle("-fx-background-insets: 0; -fx-background-color: #f0f0f0; -fx-font-size: 10px;");
         button.setMaxHeight(10);
         HBox.setMargin(button, new Insets(5,5,5,5));
 
-        label.setText(process.toString());
-        label.setFont(Font.font("Futura"));
-        label.setPrefWidth(150);
-        label.setAlignment(Pos.CENTER);
-
-        cell.getChildren().addAll(label,space,button);
-        container.getChildren().add(cell);
-
         button.setOnMouseClicked(mouseEvent -> {
             removeCell(button.getParent().getId());
         });
-    }
 
-    public void removeCell(String id){
-        Node toRemove =  container.lookup('#'+id);
-        container.getChildren().remove(toRemove);
+        return button;
     }
 
     private boolean isDark(String hexColor){
