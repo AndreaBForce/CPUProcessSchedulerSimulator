2009a737f4df800bf069065c1d59acbc51b9fbc7
matteo.metaldi@student.supsi.ch <matteo.metaldi@student.supsi.ch>
2022-01-04 09:53:47 +0100

    added import simulation

--- backend/src/main/java/utility/SimulationBackend.java
+++ backend/src/main/java/utility/SimulationBackend.java
@@ -1,5 +1,7 @@
 package utility;
 
+import service.FCFS.Process;
+
 import java.util.List;
 
 public class Simulation {
@@ -7,9 +9,18 @@ public class Simulation {
     private String algorithmName;
     private List<Process> processList;
 
+    public Simulation(String name, String algorithmName) {
+        this.name = name;
+        this.algorithmName = algorithmName;
+    }
+
     public Simulation(String name, String algorithmName, List<Process> processList) {
         this.name = name;
         this.algorithmName = algorithmName;
+        this.processList = processList;
+    }
+
+    public void setProcessList(List<Process> processList) {
         this.processList = processList;
     }
 }
